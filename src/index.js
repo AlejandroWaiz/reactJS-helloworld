a9c29db88d7fa231f8b28c6cccfbdbba00a70290
Alejandro Waiz <alejosaurio1@gmail.com>
2023-02-18 22:05:47 -0300

    more stuff incoming

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDom.createRoot(document.getElementById('root'))
+const root = ReactDom.createRoot(document.getElementById("root"));
 
-function Greetings(){
-
-    return<h1>Hello cosita preciosa</h1>
-
+function Greetings() {
+  return <h1>Hello cosita preciosa</h1>;
 }
 
-root.render(Greetings())
+root.render(Greetings());
