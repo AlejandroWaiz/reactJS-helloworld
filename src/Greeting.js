6b0419548e7d34bbf1187a34abc89b76f3076ca0
Alejandro Waiz <alejosaurio1@gmail.com>
2023-02-18 23:39:40 -0300

    Adding some exported funcs

---
+++ src/Greeting.js
@@ -0,0 +1,3 @@
+export function Greetings(){
+    return <h1>Hello there beautiful thing</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import { Greetings } from "./Greeting";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-function Greetings() {
-  return <h1>Hello cosita preciosa</h1>;
-}
 
-root.render(Greetings());
+root.render(
+    <div>
+        <Greetings/>
+    </div>
+);
